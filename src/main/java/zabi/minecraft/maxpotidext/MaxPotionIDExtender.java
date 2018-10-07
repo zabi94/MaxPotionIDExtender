330b9aac71734caf8f89dd9f410b6c4607db9b35
ZaBi94 <zabi94@gmail.com>
2018-10-07 12:47:29 +0200

    0.0.2

--- src/main/java/zabi/minecraft/maxpotidext/MaxPotionIDExtender.java
+++ src/main/java/zabi/minecraft/maxpotidext/MaxPotionIDExtender.java
@@ -25,7 +25,7 @@ import net.minecraftforge.registries.ForgeRegistry;
 public class MaxPotionIDExtender {
 	public static final String MOD_ID = "maxpotidext";
 	public static final String NAME = "MaxPotionIDExtender";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.0.2";
 	public static final String MC_VERSION = "[1.12.2,1.12.2]";
 	
 	@Instance
